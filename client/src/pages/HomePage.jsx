fe7e8ad97123af93e5fb99577a95813037c85b92
Isaque <isaquefrankli@gmail.com>
2024-03-19 21:18:17 -0300

    working on landing page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -39,7 +39,7 @@ export default function HomePage(){
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Marketing, estratégia e <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">dinheiro.</span></h1>
             <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Nós trabalhamos para aumentar o alcance da sua empresa na internet. Utilizamos técnicas únicas de marketing digital para alvancar seu negócio.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
                   <a className="btn py-3 px-4 rounded-lg text-white bg-blue-600 hover:bg-blue-700 w-full mb-4" href="#0">Contratar serviço</a>
@@ -67,7 +67,7 @@ export default function HomePage(){
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Explore the solutions</h2>
+            <h2 className="h2 text-xl mb-4">Nossos serviços e soluções</h2>
             <p className="text-xl text-gray-600">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
           </div>
 
