4ea29bfe86a48780e027d944db1f8bc7f210cea5
Isaque <isaquefrankli@gmail.com>
2024-04-23 21:00:25 -0300

    landing page stuff

--- client/src/components.jsx/LandingPage/Marketing.jsx
+++ client/src/components.jsx/LandingPage/Marketing.jsx
@@ -3,7 +3,7 @@ export default function Marketing(){
         <section className="bg-white dark:bg-gray-900">
             <div className="container px-6 py-16 mx-auto text-center">
                 <div className="max-w-lg mx-auto">
-                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Building Your Next App with our Awesome components</h1>
+                    <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Comece a trabalhar conosco e propulsione seu negócio com tecnologia.</h1>
                     <p className="mt-6 text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero similique
                         obcaecati illum mollitia.</p>
                     <button className="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -194,7 +194,7 @@ export default function HomePage(){
         </div>
     </div>
 </section>
-    <Team />
+    {/*<Team />*/}
     <Marketing />
     <Newsletter />
     </>
